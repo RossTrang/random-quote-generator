2c682f37ce92083d101272b45394d67119c24ed6
Ross <rossrtw2010@gmail.com>
2018-03-27 23:31:14 +0700

    .js now functioning

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,4 @@
-const theQuote = document.querySelector('q');
+/* const theQuote = document.querySelector('q');
 const quoteBtn = document.getElementsByClassName('newQuoteBtn');
 const tweetBtn = document.querySelector('.tweetBtn');
 
@@ -21,3 +21,29 @@ tweetBtn.onClick = function () {
   // body...
   console.log("Tweet button pressed")
 }
+*/
+
+function loadQuote() {
+ console.log("quote button pressed")
+ const xhr = new XMLHttpRequest();
+ //xhr.responseType = 'json';
+ xhr.onreadystatechange = function() {
+   if (xhr.readyState === XMLHttpRequest.DONE) {
+     theQuote.textContent = xhr.quoteText
+     console.log(xhr.quoteText)
+   }
+ };
+ xhr.open("GET", "https://crossorigin.me/http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en");
+ xhr.send();
+}
+
+function LogClick2() {
+alert("Logging");
+  console.log("Tweet button pressed");
+}
+
+var theQuote = document.querySelector('q');
+var quoteBtn = document.getElementById('newQuoteBtn');
+var tweetBtn = document.getElementById('tweetBtn');
+quoteBtn.onclick = loadQuote;
+tweetBtn.onclick = LogClick2;
