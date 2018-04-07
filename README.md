34ca805cb29ba866ec09ec69339a6ef165bd7d43
Ross <rossrtw2010@gmail.com>
2018-04-07 13:26:36 +0700

    Separated getQuote function. Now runs on page load & click.

--- README.md
+++ README.md
@@ -6,7 +6,7 @@
     2) Click a button to share the quote via Twitter (_optional_)
 
 I've set myself the goal of using no frameworks. CSS Grid will be used in place of
-Bootstrap and the native JavaScript Web API will carry out the JQuery/Ajax functions.
+Bootstrap and the native JavaScript WebAPI will carry out the JQuery/Ajax functions.
 
 The only freely public quote API I could find (that didn't have CORS issues) was
 for Ron Swanson quotes, so I've built a Ron-dom Quote Generator. FML!
--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,29 @@
-let theQuote = document.querySelector("q");
-const quoteBtn = document.getElementById("newQuoteBtn");
+let theQuote = document.querySelector('q');
+var quoteInfo = '';
+const quoteBtn = document.getElementById('newQuoteBtn');
+const tweetBtn = document.getElementById('tweet')
 
-quoteBtn.onclick = function() {
-    const xhr = new XMLHttpRequest();
-    xhr.open("GET", "https://ron-swanson-quotes.herokuapp.com/v2/quotes");
-    xhr.onload = function() {
-        if (xhr.status === 200) {
-            let quoteInfo = JSON.parse(xhr.responseText);
-            theQuote.textContent = quoteInfo;
-        } else {
-            alert("Request failed.  Returned status of " + xhr.status);
-        }
-    };
-    xhr.send();
+function getQuote() {
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', 'https://ron-swanson-quotes.herokuapp.com/v2/quotes');
+  xhr.onload = function() {
+    if (xhr.status === 200) {
+      quoteInfo = JSON.parse(xhr.responseText);
+      theQuote.textContent = quoteInfo;
+    } else {
+      alert('Request failed.  Returned status of ' + xhr.status);
+    }
+  };
+  xhr.send();
+}
+
+document.addEventListener('DOMContentLoaded', getQuote());
+
+quoteBtn.onclick = function () {
+  getQuote()
+};
+
+tweetBtn.onclick = function () {
+  this.setAttribute('href', 'https://twitter.com/intent/tweet?text=' + quoteInfo)
 };
 
